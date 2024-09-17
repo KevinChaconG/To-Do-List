9c5c0ac9fbf26f0cebd92ca3aba6d7b1e19f0515
Kevin Chacon <kevin27chacon@gmail.com>
2024-09-16 18:40:56 -0600

    Creacion Model Tareas

---
+++ To Do List/Models/Tareas.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualBasic;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_Do_List.Models
+{
+    public class Tareas
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [NotNull]
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        [NotNull]
+        public string Estado { get; set; }
+
+        [NotNull]
+        public string Prioridad { get; set; }
+
+        DateTime dtToday = DateTime.Now;
+    }
+}
