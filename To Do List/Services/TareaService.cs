4328d85faf418186cea900249f257bb1ad555170
Kevin Chacon <kevin27chacon@gmail.com>
2024-09-16 19:08:02 -0600

    Conectado a DB
    Se crearon las funciones de Obtener todo, Insertar, Actualizar y Eliminar

--- To Do List/Services/TareaService.cs
+++ To Do List/Services/TareaService.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
+using To_Do_List.Models;
 
 namespace To_Do_List.Services
 {
-    internal class TareaService
+    public class TareaService
     {
+        private readonly SQLiteConnection dbConnection;
+
+        public TareaService()
+        {
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Tareas.db3");
+
+            dbConnection = new SQLiteConnection(dbPath);
+
+            dbConnection.CreateTable<Tareas>();
+
+        }
+
+        public List<Tareas> GetAll()
+        {
+            var res = dbConnection.Table<Tareas>().ToList();
+            return res;
+        }
+
+
+        public int Insert(Tareas Tareas)
+        {
+            return dbConnection.Insert(Tareas);
+        }
+
+        public int Update(Tareas Tareas)
+        {
+            return dbConnection.Update(Tareas);
+        }
+
+        public int Delete(Tareas Tareas)
+        {
+            return dbConnection.Delete(Tareas);
+        }
     }
 }
