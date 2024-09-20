ef20daf4b56bd5897c181a3bdd72e925e1595c08
Jessica Hernandez <jessica.hernandez70@yahoo.com>
2024-09-19 18:56:44 -0600

    Proyecto terminado, aguegue estructura de addTareaForm y conectando el select opdate con el boton.

--- To Do List/Services/TareaService.cs
+++ To Do List/Services/TareaService.cs
@@ -21,12 +21,6 @@ namespace To_Do_List.Services
 
             dbConnection.CreateTable<Tareas>();
 
-            Tareas tarea = new Tareas();
-            tarea.Nombre = "Test 1";
-            tarea.Estado = "Incompleta";
-            tarea.Prioridad = "1";
-            this.Insert(tarea);
-
         }
 
         public List<Tareas> GetAll()
--- To Do List/Views/AddTareaForm.xaml.cs
+++ To Do List/Views/AddTareaForm.xaml.cs
@@ -1,15 +1,23 @@
 namespace To_Do_List.Views;
 using To_Do_List.Models;
+using To_Do_List.ViewModels;
 
 public partial class AddTareaForm : ContentPage
 {
+	private AddTareaFormViewModel viewModel;
 	public AddTareaForm()
     {
         InitializeComponent();
+		viewModel = new AddTareaFormViewModel();
+		BindingContext = viewModel;
+
+
     }
 
 	public AddTareaForm(Tareas tareas)
 	{
 		InitializeComponent();
-	}
+        viewModel = new AddTareaFormViewModel(tareas);
+        BindingContext = viewModel;
+    }
 }
